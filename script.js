cfbf4c28146bfbc562e380854f9cb7459515dc93
Nattag27 <146837442+Nattag27@users.noreply.github.com>
2023-11-18 16:45:23 +0100

    added om oss

--- script.js
+++ script.js
@@ -72,3 +72,61 @@ function removeFromCart() {
         updateCartCount(cartCount - 1);
     }
 }
+
+function openCart() {
+    // Här kan du lägga till koden för att navigera till kassan (checkout).
+    window.location.href = "checkout.html";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Hämta antalet varor från localStorage
+    var storedCartCount = localStorage.getItem('cartCount');
+
+    // Visa antalet varor på checkout-sidan
+    if (storedCartCount !== null) {
+        document.getElementById('checkoutCartCount').innerText = storedCartCount;
+    }
+});
+
+var cartCount = 0;
+
+function openCart() {
+    // Här kan du lägga till koden för att navigera till kassan (checkout).
+    window.location.href = "checkout.html";
+}
+
+// Funktion för att uppdatera antalet varor i kundvagnen
+function updateCartCount(count) {
+    cartCount = count;
+    document.getElementById('cartCount').innerText = cartCount;
+
+    // Spara antalet varor i localStorage
+    localStorage.setItem('cartCount', cartCount);
+}
+
+// Anropa denna funktion när du vill lägga till en vara i kundvagnen
+function addToCart() {
+    updateCartCount(cartCount + 1);
+
+    // Här kan du också lägga till ytterligare logik för att hantera tillägget av produkten
+    // till kundvagnen, t.ex. lagra information om de valda produkterna.
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM fully loaded. Running checkout.js...');
+
+    // Hämta antalet varor från localStorage
+    var storedCartCount = localStorage.getItem('cartCount');
+
+    console.log('Stored Cart Count:', storedCartCount);
+
+    // Visa antalet varor på checkout-sidan
+    var checkoutCartCountElement = document.getElementById('checkoutCartCount');
+
+    if (checkoutCartCountElement !== null && storedCartCount !== null) {
+        checkoutCartCountElement.innerText = storedCartCount;
+        console.log('Updated Checkout Cart Count:', storedCartCount);
+    } else {
+        console.error('Could not find checkoutCartCountElement or storedCartCount is null.');
+    }
+});
